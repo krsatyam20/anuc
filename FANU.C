4ceaffbb76fb52aaa521f1595e9b9d988ea28ad6
satyam <39213202+krsatyam20@users.noreply.github.com>
2020-01-11 18:40:54 +0530

    Add files via upload

---
+++ FANU.C
@@ -0,0 +1,23 @@
+// Add Two Any integer number
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+//int x=10;
+//int y=20;
+int a,b,z;
+clrscr();
+printf("Please enter the value of a");
+scanf("%dd ",&a);
+
+
+printf("Please enter the value of b");
+scanf("%d ",&b);
+
+z=a+b;
+printf("Add value of = %d",z);
+
+getch();
+
+}
---
+++ FIRST.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+ int x=10;
+ int roll;
+ clrscr();
+ printf("Hello Anu what is your rolno.");
+ scanf("%d",&roll);
+
+ printf("hi anu your roll no is= %d\n",roll);
+ printf("%d",x);
+
+getch();
+
+}
