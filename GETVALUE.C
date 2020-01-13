f3514379475396d73d54530f4c4ecfd54691f551
satyam <39213202+krsatyam20@users.noreply.github.com>
2020-01-13 17:02:09 +0530

    Add files via upload
    Question 1:WAP to input radius and calculate the area and circumference of a circle.
    Question 2: WAP to input length and breadth of a rectangle and calculate the area and perimeter.
    
    Question 3: WAP to input employee code, name and basic salary of an employee and calculate the following values:
    		HRA					40 % of basic salary
    		DA					10 % of basic salary
    		CCA					5 % of basic salary
    		GS					Basic + HRA + DA + CCA
    		PF					10 % of GS
    		IT					10 % of GS
    		NS					GS – (PF + IT)
    Display all the values.

---
+++ GETVALUE.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+//int a=10;  //fixed value
+//int b=20;
+
+int a,b,c;
+clrscr();
+printf("Please enter the any number");
+scanf("%d",&a);
+
+printf("Please enter the any second number");
+scanf("%d",&b);
+c=a+b;
+
+printf("add value of %d + %d = %d",a,b,c);
+
+
+
+getch();
+
+}
---
+++ Q2.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+int r,c;
+
+clrscr();
+
+printf ("Please enter any value for r");
+scanf("%d",&r);
+
+c=2*3.14*r;
+
+printf("%d",c);
+
+getch();
+}
